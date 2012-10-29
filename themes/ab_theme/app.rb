aa557c78131c1f3334cb70e9119fa923db784b70
Cody Duval <cody.duval@gmail.com>
2012-10-29 09:09:54 -0400

    Added js readmore functionality to blog

--- themes/ab_theme/app.rb
+++ themes/ab_theme/app.rb
@@ -7,7 +7,18 @@ module Nesta
     use Rack::Static, :urls => ["/ab_theme"], :root => "themes/ab_theme/public"
 
     helpers do
-      # Add new helpers here.
+      def format_date(date)
+        date.strftime("%B %d, %Y")
+      end
+
+      def show_1000_characters(body)
+        body[0..1000]
+      end
+
+      def show_characters_over_1000(body)
+        body[1001..10000]
+      end
+
     end
 
     # Add new routes here.
